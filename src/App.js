418567794d2716f7d2f252fff1dd392aaa61488e
Ken Sodemann <ken@ionic.io>
2018-07-02 15:07:28 -0500

    feat(weather): show the temperature using a React component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Temperature extends Component {
+  render() {
+    if (this.props.temperature || this.props.temperature === 0) {
+      if (this.props.scale === 'C') {
+        return <span>{(this.props.temperature - 273.15).toFixed(0)} ℃</span>;
+      } else {
+        return (
+          <span>
+            {((this.props.temperature * 9) / 5 - 459.67).toFixed(0)} ℉
+          </span>
+        );
+      }
+    }
+    return null;
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scale: 'F',
+      temperature: 298
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +36,20 @@ class App extends Component {
           <h1 className="App-title">Campsite React Shuns</h1>
         </header>
         <div className="content">
+          <div
+            className="temperature"
+            onClick={() =>
+              this.setState({
+                scale: this.state.scale === 'C' ? 'F' : 'C',
+                temperature: this.state.temperature
+              })
+            }
+          >
+            <Temperature
+              temperature={this.state.temperature}
+              scale={this.state.scale}
+            />
+          </div>
           <h2>What did you think of our campsite? Let us know...</h2>
           <h3>Recent Reviews</h3>
           <ul>
